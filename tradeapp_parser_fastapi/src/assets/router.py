fb0423eb58068f54e42481b7d039b0b0e8fd0482
Sergey M <serg.mankevi@gmail.com>
2023-07-19 19:15:37 +0300

    Added assets

--- tradeapp_parser_fastapi/src/assets/router.py
+++ tradeapp_parser_fastapi/src/assets/router.py
@@ -12,9 +12,11 @@ router = APIRouter(
 @router.post('/add_coin')
 async def add_coin(coin: Coin, db=Depends(get_database)):
     collection = db['coins']
+
     existing_coin = await collection.find_one({"coin": coin.coin})
     if existing_coin:
         raise HTTPException(status_code=400, detail="Coin already exists")
+
     coin_dict = coin.model_dump()
     await collection.insert_one(coin_dict)
     return {
