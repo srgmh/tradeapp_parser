2b0f2a97d36c2d38bc2838e5a4211c06a9915e79
Sergey M <serg.mankevi@gmail.com>
2023-07-19 09:39:33 +0300

    Connected mongo

--- tradeapp_parser_fastapi/src/database.py
+++ tradeapp_parser_fastapi/src/database.py
@@ -5,8 +5,9 @@ from motor.motor_asyncio import AsyncIOMotorClient
 
 load_dotenv(find_dotenv())
 
-url = os.environ.get('DATABASE_URL')
-client = AsyncIOMotorClient(url)
 
-database = os.environ.get('MONGO_INITDB_DATABASE')
-db = client[database]
+async def get_database() -> AsyncIOMotorClient:
+    url = os.environ.get('DATABASE_URL')
+    client = AsyncIOMotorClient(url)
+    database = os.environ.get('MONGO_INITDB_DATABASE')
+    return client[database]
--- tradeapp_parser_fastapi/src/main.py
+++ tradeapp_parser_fastapi/src/main.py
@@ -3,6 +3,6 @@ from fastapi import FastAPI
 app = FastAPI()
 
 
-@app.get('/')
-def read_root():
-    return {'Hello': 'World'}
+@app.get('/ping')
+def ping():
+    return {'ping': 'pong'}
